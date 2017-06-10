829149816cec9bee831544a7b35527cc11ecd7fd
peterzky <378096232@qq.com>
2017-06-11 07:44:06 +0800

    add pocket service

---
+++ .conkerorrc/service.js
@@ -0,0 +1,19 @@
+// speed reading
+interactive("speed-reading",
+	    "speed reading using squirt",
+	    function(I){
+		let bm = "javascript:(function(){sq=window.sq=window.sq||{};if(sq.script){sq.again();}else{sq.bookmarkletVersion='0.3.0';sq.iframeQueryParams={host:'//squirt.io',userId:'5a927f5e-4366-4843-a2a6-b57defa0c14d',};sq.script=document.createElement('script');sq.script.src=sq.iframeQueryParams.host+'/bookmarklet/frame.outer.js';document.body.appendChild(sq.script);}})();";
+		load_url_in_current_buffer(bm,I.window);
+	    });
+
+define_key(content_buffer_normal_keymap, "C-x r", "speed-reading");
+
+// pocket
+interactive("save-to-pocket",
+	    "save to pocket using bookmarklet",
+	    function(I){
+		let bm = "javascript:(function(){var%20e=function(t,n,r,i,s){var%20o=[6940144,1126310,4191818,4160922,7823142,1233813,5395102,6536813,3805032,1683259];var%20i=i||0,u=0,n=n||[],r=r||0,s=s||0;var%20a={'a':97,'b':98,'c':99,'d':100,'e':101,'f':102,'g':103,'h':104,'i':105,'j':106,'k':107,'l':108,'m':109,'n':110,'o':111,'p':112,'q':113,'r':114,'s':115,'t':116,'u':117,'v':118,'w':119,'x':120,'y':121,'z':122,'A':65,'B':66,'C':67,'D':68,'E':69,'F':70,'G':71,'H':72,'I':73,'J':74,'K':75,'L':76,'M':77,'N':78,'O':79,'P':80,'Q':81,'R':82,'S':83,'T':84,'U':85,'V':86,'W':87,'X':88,'Y':89,'Z':90,'0':48,'1':49,'2':50,'3':51,'4':52,'5':53,'6':54,'7':55,'8':56,'9':57,'\/':47,':':58,'?':63,'=':61,'-':45,'_':95,'&':38,'$':36,'!':33,'.':46};if(!s||s==0){t=o[0]+t}for(var%20f=0;f<t.length;f++){var%20l=function(e,t){return%20a[e[t]]?a[e[t]]:e.charCodeAt(t)}(t,f);if(!l*1)l=3;var%20c=l*(o[i]+l*o[u%o.length]);n[r]=(n[r]?n[r]+c:c)+s+u;var%20p=c%(50*1);if(n[p]){var%20d=n[r];n[r]=n[p];n[p]=d}u+=c;r=r==50?0:r+1;i=i==o.length-1?0:i+1}if(s==255){var%20v='';for(var%20f=0;f<n.length;f++){v+=String.fromCharCode(n[f]%(25*1)+97)}o=function(){};return%20v+'5bae689f0e'}else{return%20e(u+'',n,r,i,s+1)}};var%20t=document,n=t.location.href,r=t.title;var%20i=e(n);var%20s=t.createElement('script');s.type='text/javascript';s.src='https://getpocket.com/b/r4.js?h='+i+'&u='+encodeURIComponent(n)+'&t='+encodeURIComponent(r);e=i=function(){};var%20o=t.getElementsByTagName('head')[0]||t.documentElement;o.appendChild(s)})()";
+		load_url_in_current_buffer(bm,I.window);
+	    });
+
+define_key(content_buffer_normal_keymap, "C-x s", "save-to-pocket");
--- .conkerorrc/speedreading.js
+++
@@ -1,9 +0,0 @@
-
-interactive("speed-reading",
-	    "speed reading using squirt",
-	    function(I){
-		let bm = "javascript:(function(){sq=window.sq=window.sq||{};if(sq.script){sq.again();}else{sq.bookmarkletVersion='0.3.0';sq.iframeQueryParams={host:'//squirt.io',userId:'5a927f5e-4366-4843-a2a6-b57defa0c14d',};sq.script=document.createElement('script');sq.script.src=sq.iframeQueryParams.host+'/bookmarklet/frame.outer.js';document.body.appendChild(sq.script);}})();";
-		load_url_in_current_buffer(bm,I.window);
-	    });
-
-define_key(content_buffer_normal_keymap, "C-x r", "speed-reading");
