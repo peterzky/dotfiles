1ebd54271c6d733b39adbb0125c98fa29d711767
peterzky <peter.zky@qq.com>
2017-06-24 03:41:51 +0800

    conkeror: ublock change. mpv: add screenshot template

--- .conkerorrc/ublock.js
+++ .conkerorrc/ublock.js
@@ -2,13 +2,6 @@ interactive(
     "ublock", "Open uBlock dashboard in a new buffer",
     function (I) {
         var ublock_branch;
-        if ("@ublock0/content-policy;1" in Cc) {
-            ublock_branch = "ublock0";
-        } else if ("@ublock/content-policy;1" in Cc) {
-            ublock_branch = "ublock";
-        } else {
-            throw interactive_error("uBlock not found");
-        }
-        load_url_in_new_buffer("chrome://"+ublock_branch+"/content/dashboard.html");
+        load_url_in_new_buffer("chrome://ublock0/content/dashboard.html");
     }
 );
