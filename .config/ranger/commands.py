b8a8f98a6848f41cb3d87cab789fed8edba82256
peterzky <peter.zky@qq.com>
2017-11-01 09:08:37 +0800

    ranger: fix copy file path

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -203,3 +203,31 @@ class fzf_fasd(Command):
                 self.fm.cd(fzf_file)
             else:
                 self.fm.select_file(fzf_file)
+
+class toggle_flat(Command):
+    """
+    :toggle_flat
+
+    Flattens or unflattens the directory view.
+    """
+
+    def execute(self):
+        if self.fm.thisdir.flat == 0:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = -1
+            self.fm.thisdir.load_content()
+        else:
+            self.fm.thisdir.unload()
+            self.fm.thisdir.flat = 0
+            self.fm.thisdir.load_content()
+
+class tmsu_tag(Command):
+    """:tmsu_tag
+
+    Tags the current file with tmsu
+    """
+
+    def execute(self):
+        cf = self.fm.thisfile
+
+        self.fm.run("tmsu tag \"{0}\" {1}".format(cf.basename, self.rest(1)))
