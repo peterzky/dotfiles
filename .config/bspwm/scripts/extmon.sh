b8aea97322bec3a3917ff3f5f2bffa9f3a780712
peterzky <peter.zky@qq.com>
2018-02-16 20:45:22 +0800

    thinkpad: external monitor script

---
+++ .config/bspwm/scripts/extmon.sh
@@ -0,0 +1,8 @@
+pkill polybar
+pkill compton
+xrandr --output HDMI-1 --auto --right-of eDP-1
+bspc monitor $(bspc query -M | awk NR==2) -d 1 2 3 4 5 6 7 8 9
+bspc monitor $(bspc query -M | awk NR==1) -d 1 2 3 4 5 6 7 8 9
+MONITOR=eDP-1 polybar thinkpad &
+MONITOR=HDMI-1 polybar thinkpad &
+compton -b
