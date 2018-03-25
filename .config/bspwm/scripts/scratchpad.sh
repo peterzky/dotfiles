68018a70ee3f554bc2220e5a9402a4e1bdd1ce82
peterzky <peter.zky@qq.com>
2018-03-25 16:34:22 +0800

    emacs: font update

--- .config/bspwm/scripts/scratchpad.sh
+++ .config/bspwm/scripts/scratchpad.sh
@@ -3,4 +3,4 @@ win_class=$1
 command=$2
 xdotool search --onlyvisible --classname $win_class windowunmap \
     || xdotool search --classname $win_class windowmap \
-    || urxvt -name $win_class -e $command
+    || urxvtc -name $win_class -e $command
