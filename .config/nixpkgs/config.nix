15638f887346b369eae00442a9b2fa8159156f4f
peterzky <peter.zky@qq.com>
2018-07-15 04:41:17 +0800

    nixpkgs: add overlay

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,4 +1,9 @@
 {pkgs, ...}:
 {
   allowUnfree = true;
+  overlays = [
+    (import ../../nixos/overlays/package-overlay.nix)
+    (import ../../nixos/overlays/python-overlay.nix)
+    (import ../../nixos/overlays/env-overlay.nix)
+  ];
 }
