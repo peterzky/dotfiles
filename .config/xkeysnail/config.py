e4fbaf3f3f8fec01ae09fdfc462cc536a514aae2
peterzky <peter.zky@qq.com>
2018-12-10 04:13:43 +0800

    update

--- .config/xkeysnail/config.py
+++ .config/xkeysnail/config.py
@@ -37,7 +37,7 @@ define_keymap(re.compile("Zeal"), {
 }, "Zeal")
 
 # Emacs-like keybindings in non-Emacs applications
-define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt"), {
+define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "VirtualBox Machine"), {
     # Cursor
     K("C-b"): with_mark(K("left")),
     K("C-f"): with_mark(K("right")),
