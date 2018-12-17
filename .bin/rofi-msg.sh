409a133dbc3534ad25e6560947f0a3b7e13e4aba
peterzky <peter.zky@qq.com>
2018-12-18 06:10:00 +0800

    update rofi script

--- .bin/rofi-msg.sh
+++ .bin/rofi-msg.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-choices="Telegram\nHexChat\nVirtualBox\nMusicbox"
+choices="Telegram\nHexChat\nMusicbox\nWeChat\nCmus"
 
 chosen=$(echo -e "$choices" | rofi -dmenu -i -p MSG)
 
 case "$chosen" in
     Telegram) telegram-desktop;;
     HexChat) hexchat;;
-    VirtualBox) VirtualBox;;
-    Musicbox) appimage-run ~/Sync/sync/appimg/ieaseMusic.AppImage ;;
+    Musicbox) ieaseMusic;;
+    WeChat) wewechat ;;
+    Cmus) urxvtc -geometry 130x26-500+200 -e cmus;;
 esac
--- .bin/rofi-org.sh
+++ .bin/rofi-org.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-choices="Agenda\nTodo\nProjects\nInbox\nRSS\nEMail"
+choices="Agenda\nTodo\nProjects\nInbox\nRSS\nEMail\nwiki"
 
 chosen=$(echo -e "$choices" | rofi -dmenu -i -p ORG)
 
+wiki=$(echo $chosen | sed 's/wiki\ //')
+
 run()
 {
-    emacsclient -nc -e "$@"
+    emacsclient -nc -F "(list (name . \"ORG\"))" -e "$@"
 }
 
 
@@ -14,6 +16,7 @@ case "$chosen" in
     RSS) run "(call-interactively 'elfeed)";;
     EMail) run "(call-interactively 'mu4e)";;
     Agenda) run "(call-interactively 'org-agenda-list)";;
-    Projects) run "(find-file \"~/Sync/sync/org/agenda/project.org\")";;
-    Inbox) run "(find-file \"~/Sync/sync/org/agenda/inbox.org\")";;
+    Projects) emacsclient -nc -e "(find-file \"~/Sync/sync/org/agenda/project.org\")";;
+    Inbox) emacsclient -nc -e "(find-file \"~/Sync/sync/org/agenda/inbox.org\")";;
+    wiki*) run "(org-search-view nil \"$wiki\" nil)"
 esac
