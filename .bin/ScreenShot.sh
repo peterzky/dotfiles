1df906fab6a99c3ad2558c84b1ee966a49bfe740
peterzky <peter.zky@qq.com>
2018-12-28 20:31:48 +0800

    update screenshot script

--- .bin/ScreenShot.sh
+++ .bin/ScreenShot.sh
@@ -1,17 +1,24 @@
 #!/run/current-system/sw/bin/env bash
 
+image=/tmp/screenshot.png
+
 maim -s | xclip -selection clipboard -t image/png
 sleep 0.1
-xclip -selection clipboard -t image/png -o > /tmp/screenshot.png
+xclip -selection clipboard -t image/png -o > $image
 sleep 0.2
 
-X=`file /tmp/screenshot.png | cut -d ',' -f2 | cut -d ' ' -f2`
-Y=`file /tmp/screenshot.png | cut -d ',' -f2 | cut -d ' ' -f4`
-Xp=500
-Ypp=`echo $Y/$X*500 | bc -l | cut -d '.' -f1`
-Yp=`echo $Ypp+1 | bc`
+X=`identify $image | awk '{print $3}' | cut -dx -f1`
+Y=`identify $image | awk '{print $3}' | cut -dx -f2`
 
-feh -g 500x$Yp+1350+50 \
+Yp=500
+Xp=`echo $X/$Y*$Yp | bc -l | cut -d '.' -f1`
+
+echo $Xp $Yp
+# Xp=500
+# Ypp=`echo $Y/$X*500 | bc -l | cut -d '.' -f1`
+# Yp=`echo $Ypp+1 | bc`
+
+feh -g ${Xp}x${Yp} \
     --info 'echo "1:save  2:delete  3:search"' \
     --scale-down \
     --auto-zoom \
