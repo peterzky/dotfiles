c0d453b61a96f148ca4ea7ed0799dd10d4960f60
peterzky <peter.zky@qq.com>
2019-01-21 16:33:34 +0800

    gfw-update script fix new path; add volume script

--- .bin/gfw-update.sh
+++ .bin/gfw-update.sh
@@ -4,15 +4,15 @@ gfw_list="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
 gfw_supplement="https://raw.githubusercontent.com/hq450/fancyss/master/rules/gfwlist.conf"
 
 update_list() {
-    cat /tmp/gfw_all_domain > $HOME/nixos/files/gfw_all_domain
-    cat /tmp/china_ip_list  > $HOME/nixos/files/china_ip_list
+    cat /tmp/gfw_all_domain > $HOME/nixos/nix-config/files/gfw_all_domain
+    cat /tmp/china_ip_list  > $HOME/nixos/nix-config/files/china_ip_list
     echo "Update Completed, Please rebuild the system when possible."
 }
 
 echo "update china_ip_list..."
 curl -Ls $ip_list -o /tmp/china_ip_list
 echo "download completed"
-diff ~/nixos/files/china_ip_list /tmp/china_ip_list
+diff ~/nixos/nix-config/files/china_ip_list /tmp/china_ip_list
 
 echo "update gfw_list..."
 curl -Ls $gfw_list \
@@ -27,7 +27,7 @@ curl -Ls $gfw_supplement \
     | sed 's/ipset=\/\.//g; s/\/gfwlist//g; /^server/d' > /tmp/temp_koolshare.txt
 echo "concatenate gfw_list and gfw_supplement"
 cat /tmp/temp_gfwlist.txt /tmp/temp_koolshare.txt | sort -u > /tmp/gfw_all_domain
-diff ~/nixos/files/gfw_all_domain /tmp/gfw_all_domain
+diff ~/nixos/nix-config/files/gfw_all_domain /tmp/gfw_all_domain
 
 while true; do
     read -p "Update List?(y/n): " yn
